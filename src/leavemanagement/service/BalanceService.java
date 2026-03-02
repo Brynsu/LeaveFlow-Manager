abbe6e3b0a395e4402648557825f626587f78121
Shakira Marie Docena <shakiramariedocena@gmail.com>
2026-03-03 00:04:31 +0800

    added Balance Service class

---
+++ src/leavemanagement/service/BalanceService.java
@@ -0,0 +1,30 @@
+package leavemanagement.service;
+import leavemanagement.employee.Employee;
+public class BalanceService {
+
+    public void deductBalance(Employee emp, String leaveType, int days) {
+
+        if (emp == null) {
+            System.out.println("Employee not found.");
+            return;
+        }
+
+        if (leaveType.equalsIgnoreCase("VL")) {
+            emp.setVacationBal(emp.getVacationBal() - days);
+
+        } else if (leaveType.equalsIgnoreCase("SL")) {
+
+            emp.setSickBal(emp.getSickBal() - days);
+
+        } else if (leaveType.equalsIgnoreCase("EL")) {
+
+            emp.setEmergencyBal(emp.getEmergencyBal() - days);
+        } else {
+
+            System.out.println("Invalid leave type.");
+            return;
+        }
+
+        System.out.println("Balance deducted successfully.");
+    }
+}
